f2fbf0b2df66c200531636a5c6624f97e12ce4c1
Nick Musaelian <nmusaelian@nmusaellian-mbr.f4tech.com>
2015-06-25 17:05:51 -0600

    added language param

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require 'sinatra'
 get '/' do
   "search code in github repos"
 end
-get '/search-code/:words/:user' do
-  data = get_data(params[:words],params[:user])
+get '/search-code/:words/:language/:user' do
+  data = get_data(params[:words],params[:language],params[:user])
   "found #{data.inspect}"
 end
 
-def get_data(word, user)
+def get_data(word,language,user)
   data = Hash.new
   client = Octokit::Client.new :access_token => `echo $MYGHTOKEN` 
-  results = client.search_code("#{word} language:js user:#{user}")
+  results = client.search_code("#{word} language:#{language} user:#{user}")
   data[:total_count] = results.total_count
   data[:html_urls] = Array.new
   results.items.each do |x|
