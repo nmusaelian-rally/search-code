5228bc57234ae97b557c0cf4de820c9a822b2500
Nick Musaelian <nmusaelian@nmusaellian-mbr.f4tech.com>
2015-06-26 09:36:04 -0600

    made results clickable,redirected 404 to notfound

--- app.rb
+++ app.rb
@@ -1,12 +1,27 @@
 require 'octokit'
 require 'sinatra'
+
+enable :sessions
+
 get '/' do
   "search code in github repos"
 end
 get '/search-code/:words/:language/:user' do
   data = get_data(params[:words],params[:language],params[:user])
-  "found #{data.inspect}"
+  erb :results, :locals => {:total_count => data[:total_count], :results => data[:html_urls]}
 end
+not_found do
+  session[:message] = request.fullpath
+  redirect "/not-found"
+end
+
+get '/not-found' do
+  wrong_path = session[:message]
+  expected_syntax = "/search-code/keyword/language/account"
+  message = "Page not found. The URL should follow this format:"
+  erb :notfound, :locals => {:wrong_path => wrong_path, :message => message, :expected_syntax => expected_syntax}
+end
+
 
 def get_data(word,language,user)
   data = Hash.new
